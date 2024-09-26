05be9bcb7c839c67c33bc66b59dd70b5dc76f60b
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-09-26 18:19:53 +0530

    bottombar

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker_app/app_view.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MyAppView();
   }
 }
