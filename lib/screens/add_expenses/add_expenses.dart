1518d2e3d0972690e95de0a09e369e412333791d
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-09-29 19:52:39 +0530

    AddExpenses amount input field

--- lib/screens/add_expenses/add_expenses.dart
+++ lib/screens/add_expenses/add_expenses.dart
@@ -1,28 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AddExpenses extends StatelessWidget {
   const AddExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      // To un-focus when tap outside of text field, wrap the Entire Scaffold with GestureDetector
+      child: Scaffold(
         backgroundColor: Theme.of(context).colorScheme.surface,
-      ),
-      body: const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "Add Expenses",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w500,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Add Expenses",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 16),
+              SizedBox(
+                // Wrap the TextFormField with SizedBox with a width to take the size of SizedBox
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(
+                      FontAwesomeIcons.dollarSign,
+                      size: 18,
+                      color: Colors.grey,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextFormField(),
+              SizedBox(height: 16),
+              TextFormField(),
+              SizedBox(height: 16),
+              TextButton(
+                onPressed: () {},
+                child: Text("Save"),
+              ),
+            ],
+          ),
         ),
       ),
     );
