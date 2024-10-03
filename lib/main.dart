011add6a6e45b5c02ab337bde23396b887d2b8bb
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-10-03 19:43:26 +0530

    connected to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/app.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = SimpleBlocObserver();
   runApp(const MyApp());
 }
